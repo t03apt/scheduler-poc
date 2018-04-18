441cd2b29d1f805cbbd03a5271ed341bd6e3fa99
t03apt <t03apt@hotmail.com>
2018-04-18 21:47:49 +0200

    Add project files.

---
+++ SchedulerPOC/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SchedulerPOC
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
