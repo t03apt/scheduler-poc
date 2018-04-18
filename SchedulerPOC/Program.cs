e8f10f0f9960fa34501ccb02fc17701826b1318b
t03apt <t03apt@hotmail.com>
2018-04-18 22:16:57 +0200

    add: sample using Monitor

--- SchedulerPOC/Program.cs
+++ SchedulerPOC/Program.cs
@@ -1,12 +1,59 @@
 ﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace SchedulerPOC
 {
     class Program
     {
+        private static object lockObj = new object();
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int entityId = 0;
+            TriggerJob(entityId);
+        }
+
+        private static void TriggerJob(int entityId)
+        {
+            var states = new Dictionary<int, (DateTimeOffset scheduledAt, object args)>();
+
+            Monitor.Enter(lockObj);
+            try
+            {
+                if (!states.TryGetValue(entityId, out var state))
+                {
+                    var delay = TimeSpan.FromMinutes(1);
+                    var scheduledAt = DateTimeOffset.UtcNow.Add(delay);
+                    states.Add(entityId, (scheduledAt, new object()));
+                    Task.Run(async () =>
+                    {
+                        await Task.Delay(delay);
+                        Monitor.Enter(lockObj);
+                        try
+                        {
+                            await DoJob();
+                            states.Remove(entityId);
+                        }
+                        finally
+                        {
+
+                        }
+                    });
+                }
+            }
+            finally
+            {
+                Monitor.Exit(lockObj);
+            }
+
+        }
+
+        private static Task DoJob()
+        {
+            throw new NotImplementedException();
         }
     }
 }
